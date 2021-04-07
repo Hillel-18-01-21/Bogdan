0033ccb1ab4b4aedb79d74dc1bee0906eb41873c
Weedsurf <bogdanbabkevich@gmail.com>
2021-04-07 19:55:47 +0300

    hw17 inited

---
+++ Homework-17/script.js
@@ -0,0 +1,11 @@
+const userTemplate = document.getElementById('usersList-template');
+const mainPageTemplate = document.getElementById('mainPage-template');
+
+const loginForm = new LoginForm(onLogin);
+const mainPage = new MainPage(userTemplate, mainPageTemplate);
+function onLogin(){
+  mainPage.init();
+}
+
+// "email": "eve.holt@reqres.in",
+//   "password": "cityslicka"
